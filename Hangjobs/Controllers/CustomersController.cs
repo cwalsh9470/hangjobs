ed5ac426503392d888258b8385680a2151d62eb3
monstersatish <monstersatish@live.com>
2017-04-07 08:30:34 +0530

    Add project files.

---
+++ Hangjobs/Controllers/CustomersController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Hangjobs.Models;
+using Hangfire;
+
+namespace Hangjobs.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public CustomersController()
+        {
+
+        }
+        private HangjobsEntities db = new HangjobsEntities();
+
+        // GET: Customers
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Customers.ToListAsync());
+        }
+
+        // GET: Customers/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = await db.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(customer);
+                await db.SaveChangesAsync();
+                BackgroundJob.Enqueue(() => ConfirmUser(customer.Id));
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
+        }
+
+        public async Task<bool> ConfirmUser(int id)
+        {
+            using (var context = new HangjobsEntities())
+            {
+                var user = await context.Customers.FindAsync(id);
+                if (user == null)
+                    throw new ArgumentException(string.Format("No user found with id {0} ", id));
+                else
+                {
+                    user.IsConfirmed = true;
+                    context.Entry(user).State = EntityState.Modified;
+                    int edited = await context.SaveChangesAsync();
+                    if (edited > 0)
+                        return true;
+                    else
+                        throw new ArgumentException(string.Format("User {0} could not be confirmed.", id));
+                }
+            }
+        }
+
+        #region NIU
+        // GET: Customers/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = await db.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,IsConfirmed")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = await db.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Customer customer = await db.Customers.FindAsync(id);
+            db.Customers.Remove(customer);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        #endregion
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Hangjobs/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Hangfire;
+
+[assembly: OwinStartup(typeof(Hangjobs.Startup))]
+
+namespace Hangjobs
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            GlobalConfiguration.Configuration.UseSqlServerStorage("HangjobsConnection")
+                //.UseMsmqQueues(@"Direct=OS:localhost\hangfire-{0}")
+                   ;
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+        }
+    }
+}
